f0efe2884b41251579d1345f18779d6d35462994
ETamez9173 <elitamez17@gmail.com>
2020-04-10 16:27:44 -0500

    first commit

---
+++ DepartmentEmployees/Program.cs
@@ -0,0 +1,28 @@
+﻿using DepartmentsEmployees.Data;
+using DepartmentsEmployees.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DepartmentsEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DepartmentRepository departmentRepo = new DepartmentRepository();
+
+            Console.WriteLine("Getting All Departments:");
+            Console.WriteLine();
+
+            List<Department> allDepartments = departmentRepo.GetAllDepartments();
+
+            foreach (Department dept in allDepartments)
+            {
+                Console.WriteLine($"{dept.Id} {dept.DeptName}");
+            }
+        }
+
+
+
+    }
+}
